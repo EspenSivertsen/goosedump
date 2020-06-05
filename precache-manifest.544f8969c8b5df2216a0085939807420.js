28671f95afd38bc82033b0f1bc892e36343f1bfd
Espen Sivertsen <EspenSivertsen@users.noreply.github.com>
2020-06-05 18:20:11 +0200

    Updates

---
+++ precache-manifest.544f8969c8b5df2216a0085939807420.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd7ca5d6998f84b48cf252c5faede22a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7630e6fe398723a0f076",
+    "url": "/static/css/main.f0452b1a.chunk.css"
+  },
+  {
+    "revision": "556fae76bdade28a2663",
+    "url": "/static/js/2.5f2d9af7.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/static/js/2.5f2d9af7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7630e6fe398723a0f076",
+    "url": "/static/js/main.b85b2266.chunk.js"
+  },
+  {
+    "revision": "dc59b3363c99474f5128",
+    "url": "/static/js/runtime-main.540c7ffe.js"
+  },
+  {
+    "revision": "df1ee5a032e1c2de3aaadeb0c10099ac",
+    "url": "/static/media/2018logo4.df1ee5a0.png"
+  },
+  {
+    "revision": "06462f70da056234746670bf0232acdf",
+    "url": "/static/media/GamesTitle.06462f70.png"
+  },
+  {
+    "revision": "7315defb18de6db9c47ffd59424c277f",
+    "url": "/static/media/GoosedumpTitle.7315defb.png"
+  },
+  {
+    "revision": "683105b5da3bfd04bd5b1b0a9e97d29b",
+    "url": "/static/media/aktaer.683105b5.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "7a989133c3b90e3f309cb91ae8e80cb9",
+    "url": "/static/media/tetrisshooter.7a989133.png"
+  },
+  {
+    "revision": "979f84da8dca3be871fb0b89f4b0faf4",
+    "url": "/static/media/toppblokk.979f84da.png"
+  }
+]);
