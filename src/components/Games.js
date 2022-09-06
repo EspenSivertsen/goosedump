213605cb1572073254846c55cf31d16fd6367848
Espen Sivertsen <espen.sivertsen@trondheim.kommune.no>
2022-09-06 15:19:26 +0200

    kompetansedagen

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,53 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import 'react-responsive-modal/styles.css';
+import Modal from 'react-responsive-modal';
 
-class Games extends React.Component {
-  constructor(props){
-        super(props);
-        this.state = { open: false };
-  }
-  toggle() {
-    this.setState({
-      open: !this.state.open
-    });
-  }
-  render() {
-    return (
-      <div className="games">
-				<div class="game-box">
-					<div class="game-feature">
-						{<img src={this.props.image} class="poster" height="150px" />}
-					</div>
-					<div class="game-info">
-						<h2>{this.props.gameTitle}</h2>
-            <p>{this.props.gameDesc}</p>
-						<a href={this.props.gameUrl}>
-							<img alt='Game Platform' src={this.props.gamePlatform} height='50px' />
-            </a>
-					</div>
-				</div>
-      </div>
-      /**<div className="games2" onClick={this.toggle.bind(this)} style={{ cursor: "pointer" }}>
-        <div className="gamebox">
-          <img src={this.props.image} class="poster2" />
-          <h2>{this.props.gameTitle}</h2>
-          <h5>{this.props.gamePlatform}</h5>
+function Games(props) {
+
+  const [open, openModal] = useState(false);
+  const [liked, setLiked] = useState(false);
+
+  const toggleModal = () => {
+    openModal(!open)
+  };
+
+  return (
+    /**<div className="games">
+      <div class="game-box">
+        <div class="game-feature">
+          {<img src={this.props.image} class="poster" height="150px" />}
         </div>
-        <div id="demo" className={"collapse" + (this.state.open ? ' in' : '')}>
-          <div>
-
-              Anim pariatur cliche reprehenderit.
-               Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-               when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-               It has survived not only five centuries, but also the leap into electronic typesetting,
-               remaining essentially unchanged.
-               It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-               and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-
+        <div class="game-info">
+          <h2>{this.props.gameTitle}</h2>
+          <p>{this.props.gameDesc}</p>
+          <a href={this.props.gameUrl}>
+            <img alt='Game Platform' src={this.props.gamePlatform} height='50px' />
+          </a>
+        </div>
+      </div>
+    </div>**/
+    <div id="games">
+      <div className="game">
+        <div className="game-box">
+          <div className="game-feature">
+            {<img src={props.image} className="poster" height="150px" onClick={toggleModal}/>}
+          </div>
+            <div className="game-info">
+              <h2>{props.gameTitle}</h2>
+              <p>{props.gameDesc}</p>
+            </div>
+          </div>
+          <div className="game-footer">
+            <a href={props.gameUrl}>
+              <img alt='Game Platform' src={props.gamePlatform} height='50px' />
+            </a>
+            <button onClick={() => setLiked(!liked)}>
+              {liked ? "Liked" : "Like"}
+            </button>
           </div>
         </div>
-      </div>**/
-    );
-  }
+        <Modal open={open} onClose={toggleModal} center>
+          <h2>Simple centered modal</h2>
+        </Modal>      
+    </div>
+  );
 }
 
 export default Games;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,30 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import titleLogo from '../images/GoosedumpTitle.png';
 import logo from '../images/2018logo4.png';
 import gameLogo from '../images/GamesTitle.png';
 
 export class NavBar extends React.Component {
+
   render() {
     return (
-      <div className="navbar">
-        <div className="nav">
-          <img src={titleLogo} class="nav-title" />
-          <a href="#"> <img src={logo} class="nav-logo" /></a>
-          <img src={gameLogo} class="nav-title" />
-        </div>
-        <div id="navbar" className="nav">
-          <div className="nav-list">
-            <ul className="nav">
-              <a href="#home" className="active-nav"><li>Home</li></a>
-              <a href="#about"><li>About</li></a>
-              <a href="#projects"><li>Projects</li></a>
-              <a href="#games"><li>Games</li></a>
-              <a href="#contact"><li>Contact</li></a>
-              <a href="javascript:void(0);" className="icon" onclick="responsenav()">&#9776;</a>
-            </ul>
+      <nav>
+        <div className="navbar">
+          <div className="nav">
+            <img src={titleLogo} className="nav-title" />
+            <a href="#"> <img src={logo} className="nav-logo" /></a>
+            {/* <img src={gameLogo} class="nav-title" /> */}
+          </div>
+          <div id="navbar" className="nav">
+            <div className="nav-list">
+              <ul className="nav">
+                <a href="#home" className="active-nav"><li>Home</li></a>
+                <a href="#about"><li>About</li></a>
+                <a href="#projects"><li>Projects</li></a>
+                <a href="#games"><li>Games</li></a>
+                <a href="#contact"><li>Contact</li></a>
+              </ul>
+              <button className="hamburger"></button>
+            </div>
           </div>
         </div>
-      </div>
+      </nav>
     );
   }
 }
